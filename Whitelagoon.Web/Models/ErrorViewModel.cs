82c9ea8b27d35206c3aeade113f9887a7e5eefa8
mm-hem <mm.hemnavadia@gmail.com>
2024-12-30 18:14:55 +0530

    Add project files.

---
+++ Whitelagoon.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Whitelagoon.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
