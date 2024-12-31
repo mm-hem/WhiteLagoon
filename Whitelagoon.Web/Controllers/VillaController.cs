9b805c859c4e76ca43da709fcff49efadc976a82
mm-hem <mm.hemnavadia@gmail.com>
2024-12-31 11:00:34 +0530

    Part 1 Changes

---
+++ Whitelagoon.Web/Controllers/VillaController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WhiteLagoon.Infrastructure.Data;
+
+namespace Whitelagoon.Web.Controllers
+{
+    public class VillaController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public VillaController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            var Villas = _db.Villas.ToList();
+            return View(Villas);
+        }
+
+        public IActionResult Create() 
+        {
+            return View();
+        }
+    }
+}
