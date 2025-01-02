ca26fddfcf38ad058b76a4ff8ab7e059c343c0a7
mm-hem <mm.hemnavadia@gmail.com>
2025-01-02 12:23:17 +0530

    Home Page created and Changed nav bar.

---
+++ WhiteLagoon.Web/ViewModel/HomeVM.cs
@@ -0,0 +1,13 @@
+﻿using WhiteLagoon.Domain.Entities;
+
+namespace WhiteLagoon.Web.ViewModel
+{
+    public class HomeVM
+    {
+        public IEnumerable<Villa>? VillaList { get; set; }
+        public DateOnly CheckInDate { get; set; }   
+        public DateOnly CheckOutDate { get; set; }
+        public int Nights { get; set; } 
+
+    }
+}
--- Whitelagoon.Web/Controllers/HomeController.cs
+++ Whitelagoon.Web/Controllers/HomeController.cs
@@ -1,19 +1,26 @@
 using Microsoft.AspNetCore.Mvc;
+using WhiteLagoon.Application.Common.Interfaces;
+using WhiteLagoon.Web.ViewModel;
 
 namespace WhiteLagoon.Web.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
+        private readonly IUnitOfWork _unitOfWork;
+        public HomeController(IUnitOfWork unitOfWork)
+        { 
+            _unitOfWork = unitOfWork;
         }
 
         public IActionResult Index()
         {
-            return View();
+            HomeVM homeVM = new()
+            {
+                VillaList = _unitOfWork.Villa.GetAll(includeProperties: "VillaAmenity"),
+                Nights = 1,
+                CheckInDate = DateOnly.FromDateTime(DateTime.Now)                
+            };
+            return View(homeVM);
         }
 
         public IActionResult Privacy()
